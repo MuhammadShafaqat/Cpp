7bbc338591a8e5c359675a91a0549b347bf67ffb
Muhammad Shafaqat <muhammadshafaqat0344@gmail.com>
2022-09-30 11:35:50 +0500

    c++ start practice

---
+++ counting.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+ int main() {
+   int no;
+   cout<<"Enter the number: ";
+   
+   cin>>no;
+   int no1 = no/10;
+   int no2 = no%10;
+   
+   if(no == 0){
+   	cout<<"Zero";
+   }
+   if(no == 1){
+   	cout<<"One";
+   }
+   if(no == 2){
+   	cout<<"Two";
+   }
+    if(no == 3){
+   	cout<<"Three";
+   }
+   if(no == 4){
+   	cout<<"Four";
+   }
+      if(no == 5){
+   	cout<<"Five";
+   }
+   if(no == 6){
+   	cout<<"Six";
+   }
+    if(no == 7){
+   	cout<<"Seven";
+   }
+   if(no == 8){
+   	cout<<"Eight";
+   }
+      if(no == 9){
+   	cout<<"Nine";
+   }
+   if(no == 10){
+   	cout<<"Ten";
+   }
+   if(no == 11){
+   	cout<<"Eleven";
+   }
+   if(no == 12){
+   	cout<<"Twelve";
+   }
+    if(no == 13){
+   	cout<<"Thirteen";
+   }
+   if(no == 14){
+   	cout<<"Fourteen";
+   }
+      if(no == 15){
+   	cout<<"Fifteen";
+   }
+   if(no == 16){
+   	cout<<"Sixteen";
+   }
+    if(no == 17){
+   	cout<<"Seventeen";
+   }
+   if(no == 18){
+   	cout<<"Eighteen";
+   }
+      if(no == 19){
+   	cout<<"Ninteen";
+   }
+   if(no1 == 2){
+   	cout<<"Twenty"; 	
+   }
+   if(no1 == 3){
+   	cout<<"Thirty";
+   }
+   if(no1 == 4 ){
+   	cout<<"Fourty";
+   }
+    if(no1 == 5){
+   	cout<<"Fifty";
+   }
+   if(no1 == 6){
+   	cout<<"Sixty";
+   }
+    if(no1 == 7){
+   	cout<<"Seventy";
+   }
+   if(no1 == 8){
+   	cout<<"Eighty";
+   }
+    if(no1 == 9){
+   	cout<<"Ninty";
+   } 
+   
+   if(no2 == 1){
+   	cout<<"One";
+   }
+   if(no2 == 2){
+   	cout<<"Two";
+   }
+    if(no2 == 3){
+   	cout<<"Three";
+   }
+   if(no2 == 4){
+   	cout<<"Four";
+   }
+      if(no2 == 5){
+   	cout<<"Five";
+   }
+   if(no2 == 6){
+   	cout<<"Six";
+   }
+    if(no2 == 7){
+   	cout<<"Seven";
+   }
+   if(no2 == 8){
+   	cout<<"Eight";
+   }
+      if(no2 == 9){
+   	cout<<"Nine";
+   }
+  
+   
+
+   return 0;
+}
---
+++ while1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//	initialization
+//	while(condition) {
+//	body of loop
+//	increment/decrement
+//	}
+
+
+	int i = 5;
+	while(i>0) {
+		cout<<i<<endl;
+		i--;
+	}
+	
+//	write a program that takes character from user until user press n;
+	
+	return 0;
+}
